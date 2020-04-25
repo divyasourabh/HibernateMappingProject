80ec6213c8aea05337decc48b3f1e2d642d4669a
divyasourabh <divyasourabh@gmail.com>
2020-04-25 19:23:31 +0530

    Delete One to one mapping

---
+++ src/com/ds/hibernate/app/One2OneMappingDeleteDemoApp.java
@@ -0,0 +1,49 @@
+package com.ds.hibernate.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ds.hibernate.entity.Instructor;
+import com.ds.hibernate.entity.InstructorDetail;
+
+public class One2OneMappingDeleteDemoApp {
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {			
+			int id =1;
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			Instructor instructor = session.get(Instructor.class, id);
+					
+
+//			****************************************
+//			Note: this will ALSO Delete the details Object
+//			because of CascadeType ALL
+//			***************************
+			
+			if (instructor != null) {
+				session.delete(instructor);
+			}
+// commit transaction
+
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+}
---
+++ src/com/ds/hibernate/app/One2OneMappingDemoApp.java
@@ -0,0 +1,58 @@
+package com.ds.hibernate.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ds.hibernate.entity.Instructor;
+import com.ds.hibernate.entity.InstructorDetail;
+
+public class One2OneMappingDemoApp {
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {			
+			
+			Instructor instructor = new Instructor("Divya", "Sourabh", "divyasourabh@gmail.com");
+			
+			InstructorDetail instructorDetail = new InstructorDetail("youtube channel url", "Reading");
+			
+			instructor.setInstructorDetail(instructorDetail);
+
+			Instructor instructor1 = new Instructor("Sourabh", "DIvya", "divyasourabh@gmail.com");
+			
+			InstructorDetail instructorDetail1 = new InstructorDetail("youtube channel url", "Coding");
+			
+			instructor1.setInstructorDetail(instructorDetail1);
+			
+			
+			// start a transaction
+			session.beginTransaction();
+
+//			****************************************
+//			Note: this will ALSO save the details Object
+//			because of CascadeType ALL
+//			***************************
+			
+			
+			session.save(instructor);
+			session.save(instructor1);
+
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+}
